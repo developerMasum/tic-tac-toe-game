436db72b36e8c3030ccb7275e96f2b6c3302332b
developerMasum <developer.masum2001@gmail.com>
2024-07-30 12:27:36 +0600

    fixed draw error

--- app/page.tsx
+++ app/page.tsx
@@ -5,34 +5,32 @@ import "react-toastify/dist/ReactToastify.css";
 import Square from "./components/main";
 
 function Home() {
-  const [turn, setturn] = useState(1);
-  const [winner, setwinner] = useState(0);
-  const [moves, setmoves] = useState(0);
-  const [color, setcolor] = useState("#dd8e6f");
-  let initTable = [
+  const [turn, setTurn] = useState(1);
+  const [winner, setWinner] = useState(0);
+  const [moves, setMoves] = useState(0);
+  const [color, setColor] = useState("#dd8e6f");
+  const [tableArray, setTableArray] = useState([
     [0, 0, 0],
     [0, 0, 0],
     [0, 0, 0],
-  ];
-
-  const [tableArray, setTableArray] = useState(initTable);
-  const [gamerunning, setgamerunning] = useState(true);
+  ]);
+  const [gameRunning, setGameRunning] = useState(true);
 
   // Load sound effects
   const player1Sound = new Audio("/assets/m1.wav"); // Adjust the path according to your directory structure
   const player2Sound = new Audio("/assets/m2.wav");
   const winSound = new Audio("/assets/win.mp3");
   const drawSound = new Audio("/assets/over.mp3");
-  console.log(moves);
+
   useEffect(() => {
-    if (!gamerunning) {
-      if (moves !== 9) {
+    if (!gameRunning) {
+      if (winner) {
         toast.success(`Player ${winner === 1 ? "X" : "O"} is the winner`, {
           position: "bottom-center",
           autoClose: 3000,
         });
         winSound.play();
-      } else if (moves === 9) {
+      } else {
         toast.success("The match ended in a draw", {
           position: "bottom-center",
           autoClose: 3000,
@@ -44,84 +42,86 @@ function Home() {
         Reset();
       }, 3000);
     }
-  }, [gamerunning, moves, winner]);
+  }, [gameRunning, winner]);
 
   function Reset() {
-    initTable = [
+    setTableArray([
       [0, 0, 0],
       [0, 0, 0],
       [0, 0, 0],
-    ];
-    setTableArray(initTable);
-    setgamerunning(true);
-    setmoves(0);
-    setwinner(0);
-    setturn(1);
-    setcolor("#de8f70");
+    ]);
+    setGameRunning(true);
+    setMoves(0);
+    setWinner(0);
+    setTurn(1);
+    setColor("#de8f70");
   }
 
-  function checkArray(arr: any) {
-    if (
-      (arr[0][0] === 1 && arr[1][1] === 1 && arr[2][2] === 1) ||
-      (arr[0][0] === 2 && arr[1][1] === 2 && arr[2][2] === 2)
-    ) {
-      setwinner(turn);
-      return false;
-    }
-
+  function checkArray(arr: number[][]) {
+    // Check rows, columns, and diagonals
     for (let i = 0; i < 3; i++) {
       if (
         (arr[i][0] === 1 && arr[i][1] === 1 && arr[i][2] === 1) ||
         (arr[i][0] === 2 && arr[i][1] === 2 && arr[i][2] === 2)
       ) {
-        setwinner(turn);
+        setWinner(turn);
         return false;
-      } else if (
+      }
+
+      if (
         (arr[0][i] === 1 && arr[1][i] === 1 && arr[2][i] === 1) ||
         (arr[0][i] === 2 && arr[1][i] === 2 && arr[2][i] === 2)
       ) {
-        setwinner(turn);
+        setWinner(turn);
         return false;
       }
+    }
+
+    if (
+      (arr[0][0] === 1 && arr[1][1] === 1 && arr[2][2] === 1) ||
+      (arr[0][0] === 2 && arr[1][1] === 2 && arr[2][2] === 2)
+    ) {
+      setWinner(turn);
+      return false;
     }
 
     if (
       (arr[0][2] === 1 && arr[1][1] === 1 && arr[2][0] === 1) ||
       (arr[0][2] === 2 && arr[1][1] === 2 && arr[2][0] === 2)
     ) {
-      setwinner(turn);
+      setWinner(turn);
       return false;
-    } else {
-      return true;
     }
+
+    return true;
   }
 
   function onSquareClick(row: any, col: any) {
-    if (!gamerunning || tableArray[row][col] !== 0) return;
+    if (!gameRunning || tableArray[row][col] !== 0) return;
 
     const newTable = [...tableArray];
     newTable[row][col] = turn;
     setTableArray(newTable);
 
     const hasWinner = checkArray(newTable);
-    setgamerunning(hasWinner);
-
-    if (moves === 9) {
-      setgamerunning(false);
+    if (!hasWinner) {
+      setGameRunning(false);
+      return;
     }
 
-    if (gamerunning) {
-      const m = moves + 1;
-      setmoves(m);
-      if (moves % 2 === 0) {
-        setturn(2);
-        setcolor("#3f7cab");
-        player2Sound.play(); // Play sound for Player 2
-      } else {
-        setturn(1);
-        setcolor("#dd8e6f");
-        player1Sound.play(); // Play sound for Player 1
-      }
+    if (moves === 8) {
+      setGameRunning(false);
+      return;
+    }
+
+    setMoves(moves + 1);
+    setTurn(turn === 1 ? 2 : 1);
+    setColor(turn === 1 ? "#3f7cab" : "#dd8e6f");
+
+    if (turn === 1) {
+      player1Sound.play();
+    } else {
+      player2Sound.play();
     }
   }
 
@@ -142,75 +142,20 @@ function Home() {
         </h2>
         <div className="tableWrapper flex justify-center">
           <div className="bg-white flex flex-col justify-center">
-            <div className="flex w-auto justify-center">
-              <Square
-                onfunctionClick={onSquareClick}
-                value={tableArray[0][0]}
-                row={0}
-                col={0}
-                gamerunning={gamerunning}
-              />
-              <Square
-                onfunctionClick={onSquareClick}
-                value={tableArray[0][1]}
-                row={0}
-                col={1}
-                gamerunning={gamerunning}
-              />
-              <Square
-                onfunctionClick={onSquareClick}
-                value={tableArray[0][2]}
-                row={0}
-                col={2}
-                gamerunning={gamerunning}
-              />
-            </div>
-            <div className="flex w-auto justify-center">
-              <Square
-                onfunctionClick={onSquareClick}
-                value={tableArray[1][0]}
-                row={1}
-                col={0}
-                gamerunning={gamerunning}
-              />
-              <Square
-                onfunctionClick={onSquareClick}
-                value={tableArray[1][1]}
-                row={1}
-                col={1}
-                gamerunning={gamerunning}
-              />
-              <Square
-                onfunctionClick={onSquareClick}
-                value={tableArray[1][2]}
-                row={1}
-                col={2}
-                gamerunning={gamerunning}
-              />
-            </div>
-            <div className="flex w-auto justify-center">
-              <Square
-                onfunctionClick={onSquareClick}
-                value={tableArray[2][0]}
-                row={2}
-                col={0}
-                gamerunning={gamerunning}
-              />
-              <Square
-                onfunctionClick={onSquareClick}
-                value={tableArray[2][1]}
-                row={2}
-                col={1}
-                gamerunning={gamerunning}
-              />
-              <Square
-                onfunctionClick={onSquareClick}
-                value={tableArray[2][2]}
-                row={2}
-                col={2}
-                gamerunning={gamerunning}
-              />
-            </div>
+            {[0, 1, 2].map((row) => (
+              <div key={row} className="flex w-auto justify-center">
+                {[0, 1, 2].map((col) => (
+                  <Square
+                    key={col}
+                    onfunctionClick={onSquareClick}
+                    value={tableArray[row][col]}
+                    row={row}
+                    col={col}
+                    gamerunning={gameRunning}
+                  />
+                ))}
+              </div>
+            ))}
           </div>
         </div>
         <div className="flex justify-center p-5">
